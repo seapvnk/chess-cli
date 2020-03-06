20fe73fdc8871662a00bfb9b397d6d80ff5496b2
Pedro <www.psro@gmail.com>
2020-03-06 14:35:35 -0300

    Queen bugfix - Diagonal movement

--- src/chess/pieces/Queen.java
+++ src/chess/pieces/Queen.java
@@ -60,7 +60,7 @@ public class Queen extends ChessPiece {
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 			moves[p.getRow()][p.getColumn()] = true;
 		}
-		
+
 		// nw
 		p.setValues(position.getRow() - 1, position.getColumn() - 1);
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
@@ -82,7 +82,7 @@ public class Queen extends ChessPiece {
 		}
 
 		// se
-		p.setValues(position.getRow() - 1, position.getColumn() + 1);
+		p.setValues(position.getRow() + 1, position.getColumn() + 1);
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			moves[p.getRow()][p.getColumn()] = true;
 			p.setValues(p.getRow() + 1, p.getColumn() + 1);
@@ -92,7 +92,7 @@ public class Queen extends ChessPiece {
 		}
 
 		// sw
-		p.setValues(position.getRow() - 1, position.getColumn() + 1);
+		p.setValues(position.getRow() + 1, position.getColumn() - 1);
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			moves[p.getRow()][p.getColumn()] = true;
 			p.setValues(p.getRow() + 1, p.getColumn() - 1);
